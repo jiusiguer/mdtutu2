c09efd9674ff0c747427e5c8b1a43da68979fa9e
jiusiguer <96696511+jiusiguer@users.noreply.github.com>
2025-03-20 02:29:39 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { Toaster } from 'react-hot-toast'
+import App from './App'
+import './index.css'
+
+// 创建React Query客户端
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      <Toaster position="top-right" />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster 
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: '#ffffff',
+              color: '#333333',
+            },
+          }}
+        />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
